66038bbedb9702b48b74c662ba5f2c4dcc69ceeb
Jonathan Montoya <montoya0396@gmail.com>
2020-09-09 16:33:34 -0400

    added front-end code for email feature

--- src/Pages/contact.js
+++ src/Pages/contact.js
@@ -21,6 +21,31 @@ export default class Contact extends React.Component {
         }
     }
 
+    handleChange = (event) =>
+    {
+
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+
+    }
+
+    handleSubmit = (event) =>
+    {
+        event.preventDefault();
+
+        this.setState({
+            disabled: true,
+            emailSent: true
+        })
+
+
+    }
+
     render() {
         
         return (
@@ -46,34 +71,37 @@ export default class Contact extends React.Component {
                             <br/>
                             <br/>
 
-                            <Form>
+                            <Form onSubmit={this.handleSubmit}>
 
                                 <Form.Row className="justify-content-around">
-                                    <Form.Group as={Col} xs={8} md={4} controlId="formGridName">
-                                        <Form.Label className="text-white pl-1">Name</Form.Label>
-                                        <Form.Control type="text" placeholder="Name" required={true}/>
+                                    <Form.Group as={Col} xs={8} md={4}>
+                                        <Form.Label htmlFor="fullName" className="text-white pl-1">Full Name</Form.Label>
+                                        <Form.Control id="fullName" name="name" value={this.state.name} type="text" placeholder="Name" required={true} onChange={this.handleChange}/>
                                     </Form.Group>
 
-                                    <Form.Group as={Col} xs={8} md={4} controlId="formGridEmail">
-                                        <Form.Label className="text-white pl-1">Email</Form.Label>
-                                        <Form.Control type="email" placeholder="Email Address" required={true} />
+                                    <Form.Group as={Col} xs={8} md={4}>
+                                        <Form.Label htmlFor="email" className="text-white pl-1">Email</Form.Label>
+                                        <Form.Control id="email" name="email" type="email" value={this.state.email} placeholder="Email Address" required={true} onChange={this.handleChange} />
                                     </Form.Group>
                                 </Form.Row>
 
                                 <Form.Row className="justify-content-around">
 
-                                    <Form.Group as={Col} xs={8} md={10} controlId="formGridMessage">
-                                        <Form.Label className="text-white pl-1">Message</Form.Label>
-                                        <Form.Control className="contactTextarea" type="text" required={true}/>
+                                    <Form.Group as={Col} xs={8} md={10}>
+                                        <Form.Label htmlFor="message" className="text-white pl-1">Message</Form.Label>
+                                        <Form.Control id="message" name="message" value={this.state.message} as="textarea" rows="12" required={true} onChange={this.handleChange}/>
                                     </Form.Group>
 
                                 </Form.Row>
 
                                 <Row className="justify-content-center mt-2">
-                                        <Button type="submit" className="submitBtn">
+                                        <Button type="submit" className="submitBtn" disabled={this.state.disabled}>
                                             Send
                                         </Button>
                                 </Row>
+
+                                {this.state.emailSent === true && <p className="success-msg" >Email Sent</p> }
+                                {this.state.emailSent === false && <p className="err-msg" >Email Not Sent</p> }
 
                             </Form>
 
